2d72f0a22fb32a464db1766d74ad448a50fd7ca4
ryanz923 <50994589+ryanz923@users.noreply.github.com>
2019-05-24 09:54:01 -0500

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+client_id = '' # Generated from Google Developers API site
+client_secret = # Generated from Google Developers API site
+redirect_uri = # Generated from Google Developers API site
+access_code = # Comes from first run of connect.py
+access_token = # Comes from second run of connect.py
+refresh_token = # Comes from second run of connect.py
